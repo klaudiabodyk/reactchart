a0875b94c077abf84b8b545b283c5fdb29db52fa
Klaudia <klaudia.bodyk@icloud.com>
2022-12-09 20:40:04 +0100

    Add initial app configuration

---
+++ src/App.tsx
@@ -0,0 +1,76 @@
+import React,{ Component } from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import { faker } from '@faker-js/faker';
+import * as d3 from 'd3';
+import history from './data/history.csv';
+
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top' as const,
+    },
+    title: {
+      display: true,
+      text: 'Chart.js Line Chart',
+    },
+  },
+};
+
+// d3.csv("/history.csv").then(function(data){
+//   for (var i = 0; i < data.length; i++) {
+//     console.log(data[i].time);
+//     console.log(data[i].date);
+//   }
+//   });
+
+d3.csv(history).then(function(data: any ) { 
+  console.log(data);
+  const labels = [data.date];
+ });
+
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      borderColor: 'rgb(255, 99, 132)',
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Dataset 2',
+      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      borderColor: 'rgb(53, 162, 235)',
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
+
+export function App() {
+  return <Line options={options} data={data} />;
+}
